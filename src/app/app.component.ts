297a472ca3f8c76fb2ac0c1e3696a52691b1b784
abanoubsamir2811 <abanoubsamir2811@gmail.com>
2025-10-31 15:04:49 +0200

    Updated register form design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
